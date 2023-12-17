884257151e53bd356adedd2a6c03e3070b322e85
seiccn <ness.stephanie@gmail.com>
2023-12-17 12:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 from scipy.stats import normaltest
 
 from data_loading import load_solar_data, load_weather_data
-from visualization import plot_dc_power, plot_daily_yield, plot_ambient_temperature
+from visualization import plot_dc_power, plot_daily_yield, plot_ambient_temperature, plot_correlation_heatmap, run_statistical_tests
 
 def main():
     # Load data
@@ -18,6 +18,13 @@ def main():
     plot_dc_power(plant1_data)
     plot_daily_yield(plant1_data)
     plot_ambient_temperature(plant1_sensor)
+
+    # Correlation analysis
+    correlation_df = plant1_data[['DC_POWER', 'AC_POWER', 'AMBIENT_TEMPERATURE', 'MODULE_TEMPERATURE', 'IRRADIATION']]
+    plot_correlation_heatmap(correlation_df)
+
+    # Statistical tests
+    run_statistical_tests(plant1_data['DC_POWER'], plant1_data['AC_POWER'])
 
 if __name__ == "__main__":
     main()
