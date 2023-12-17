cf8f748de511b9bd1d345e09e1428043909154f9
seiccn <ness.stephanie@gmail.com>
2023-12-17 12:51:31 +0530

    Update data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -3,19 +3,41 @@
 import pandas as pd
 
 def load_solar_data(file_path):
-    # Load solar power generation data
-    plant1_data = pd.read_csv(file_path)
-    
-    # Data cleaning and preprocessing (as in the notebook)
-    # ...
-
-    return plant1_data
+    # Load solar generation data
+    solar_data = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return solar_data
 
 def load_weather_data(file_path):
     # Load weather sensor data
-    plant1_sensor = pd.read_csv(file_path)
+    weather_data = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return weather_data
 
-    # Data cleaning and preprocessing (as in the notebook)
-    # ...
+def load_irradiance_data(file_path):
+    # Load irradiance data
+    irradiance_data = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return irradiance_data
 
-    return plant1_sensor
+def load_module_temperature_data(file_path):
+    # Load module temperature data
+    module_temp_data = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return module_temp_data
+
+def load_solar_data_plant2(file_path):
+    # Load solar generation data for Plant 2
+    solar_data_plant2 = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return solar_data_plant2
+
+def load_weather_data_plant2(file_path):
+    # Load weather sensor data for Plant 2
+    weather_data_plant2 = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return weather_data_plant2
+
+def load_irradiance_data_plant2(file_path):
+    # Load irradiance data for Plant 2
+    irradiance_data_plant2 = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return irradiance_data_plant2
+
+def load_module_temperature_data_plant2(file_path):
+    # Load module temperature data for Plant 2
+    module_temp_data_plant2 = pd.read_csv(file_path, parse_dates=['DATE_TIME'])
+    return module_temp_data_plant2
