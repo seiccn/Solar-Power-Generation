5f44848509a3fbd1c140a1ea76ecffd30d79c9ba
seiccn <ness.stephanie@gmail.com>
2023-12-17 12:50:12 +0530

    Create visualization.py

---
+++ visualization.py
@@ -0,0 +1,18 @@
+# visualization.py
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from scipy.stats import normaltest
+
+def plot_dc_power(data):
+    # Visualization for DC Power (as in the notebook)
+    # ...
+
+def plot_daily_yield(data):
+    # Visualization for Daily Yield (as in the notebook)
+    # ...
+
+def plot_ambient_temperature(data):
+    # Visualization for Ambient Temperature (as in the notebook)
+    # ...
